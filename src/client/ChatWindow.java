57912319db54b360524a03f4395a80adc3180859
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2024-11-08 12:45:23 +0100

    Chatwindow design

--- src/client/ChatWindow.java
+++ src/client/ChatWindow.java
@@ -1,10 +1,55 @@
 package client;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-public class ChatWindow extends JFrame {
+public class ChatWindow extends JFrame implements ActionListener {
 
-    public ChatWindow() {
+    JTextArea chatArea;
+    JScrollPane chatScroll;
+    JButton connectButton;
+    JTextField messageField;
 
+    public ChatWindow() throws UnknownHostException {
+        super("Chat App");
+        this.setLayout(new BorderLayout());
+
+        InetAddress myAddress = InetAddress.getLocalHost();
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+
+        chatScroll = new JScrollPane(chatArea);
+
+        connectButton = new JButton("Connect");
+        messageField = new JTextField();
+        //messageField.addActionListener(new Sender("25.16.67.47", 1234, messageField, chatArea));
+        //connectButton.addActionListener(this);
+
+        this.add(connectButton, BorderLayout.NORTH);
+        this.add(chatScroll, BorderLayout.CENTER);
+        this.add(messageField, BorderLayout.SOUTH);
+
+        this.setDefaultCloseOperation(ChatWindow.EXIT_ON_CLOSE);
+        this.setSize(300, 400);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+   @Override
+    public void actionPerformed(ActionEvent e) {
+       /* Listener listener = null;
+        try {
+            listener = new Listener("25.16.11.103", 1234, chatArea);
+            Thread listenerThread = new Thread(listener);
+            listenerThread.start();
+        } catch (UnknownHostException ex) {
+            throw new RuntimeException(ex);
+        }*/
     }
 }
