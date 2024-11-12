6f18668316c71868c0c2260357fce014eca2e176
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2024-11-12 15:27:56 +0100

    new database

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,11 @@
+package Database;
+
+import java.net.URL;
+import java.sql.Connection;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:sqlite:/home/linnedvard/Appdatabase.db";
+    private Connection connection;
+
+}
