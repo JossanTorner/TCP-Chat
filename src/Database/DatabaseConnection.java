f12c82571a537acb0c5228ff92a7ff6f963fe87c
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2024-11-13 09:48:24 +0100

    new database connection

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -2,10 +2,15 @@ package Database;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:sqlite:/home/linnedvard/Appdatabase.db";
-    private Connection connection;
+    Connection connection = DriverManager.getConnection(URL);
 
+    public DatabaseConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(URL);
+    }
 }
